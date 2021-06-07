1931147be56482e27731c801970ef5cfe2e3bd51
hctrgznlz <hectorgzz89@gmail.com>
2021-06-07 15:36:09 -0400

    wrapped characters in span, first span selected given class, changed bg colo

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const typingDiv = document.getElementById("typing");
+const typingDiv = document.getElementById('typing');
 console.log(typingDiv);
 
 const text = 'I am the son and the heir of a shyness that is criminally vulgar. I am the son and heir of nothing in particular'
 
-typingDiv.innerText = text;
+const characters = text.split('').map((char) => {
 
+    const span = document.createElement('span');
+    span.innerText = char;
+    typingDiv.appendChild(span);
+    return span
+});
+
+const firstCharacter = characters [0];
+firstCharacter.classList.add('bg')
 
