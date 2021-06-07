26c6e5f7f46705c4f0f575e3fc5fe4b85e53fef3
hctrgznlz <hectorgzz89@gmail.com>
2021-06-07 15:11:28 -0400

    innitial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const typingDiv = document.getElementById("typing");
+console.log(typingDiv);
+
+const text = 'I am the son and the heir of a shyness that is criminally vulgar. I am the son and heir of nothing in particular'
+
+typingDiv.innerText = text;
+
+
